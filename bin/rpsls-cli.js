f5fbf6d8dfbb0ceda6c7c9cc03f2767a34cf664a
Luis Sanchez <89441865+SiulandCheese@users.noreply.github.com>
2023-05-04 17:28:14 -0400

    Should be done with bin

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -2,3 +2,30 @@ import minimist from "minimist";
 import { rpsls } from "./lib/rpsls.js"
 
 var args = minimist(process.argv.slice(2));
+
+const help_message = `Usage: node-rps [SHOT]
+Play Rock Paper Scissors Lizard Spock(RPSLS)
+
+    -h, --help      display this help message and exit
+    -r, --rules     display the rules and exit
+                
+Examples:
+    node-rps        Return JSON with single player RPSLS result.
+                        e.g. {"player":"rock"}
+     node-rps rock   Return JSON with results for RPSLS played against a simulated opponent.
+                        e.g {"player":"lizard","opponent":"spock","result":"win"}`
+
+const rules = `Rules for Rock Paper Scissors:
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock CRUSHES Scissors`; 
+
+if (args.h || args.help) {
+	console.log(help_message);
+	process.exit(0);
+}
+
+if (args.r || args.rules) {
+	console.log(rules);
+	process.exit(0);
+}
