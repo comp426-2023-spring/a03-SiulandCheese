2abed945abdcba07b0e412756d335378776e1781
Luis Sanchez <89441865+SiulandCheese@users.noreply.github.com>
2023-05-06 15:23:36 -0400

    Changed around the help and setup messages

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -5,7 +5,7 @@ import { rps } from "../lib/rpsls.js"
 var args = minimist(process.argv.slice(2));
 
 const help_message = `Usage: node-rps [SHOT]
-Play Rock Paper Scissors (RPSLS)
+Play Rock Paper Scissors (RPS)
 
   -h, --help      display this help message and exit
   -r, --rules     display the rules and exit
@@ -16,18 +16,10 @@ Examples:
   node-rps rock   Return JSON with results for RPS played against a simulated opponent.
                   e.g {"player":"rock","opponent":"scissors","result":"win"}`; 
 
-const rules = `Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
-- Scissors CUTS Paper
-- Paper COVERS Rock
-- Rock SMOOSHES Lizard
-- Lizard POISONS Spock
-- Spock SMASHES Scissors
-- Scissors DECAPITATES Lizard
-- Lizard EATS Paper
-- Paper DISPROVES Spock
-- Spock VAPORIZES Rock
-- Rock CRUSHES Scissors
-`;
+const rules = `Rules for Rock Paper Scissors:
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock CRUSHES Scissors`; 
 
 if (args.h || args.help) {
 	console.log(help_message);
